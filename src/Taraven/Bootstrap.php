ab5d48c246b00f0c7c9f253ca2d7210ef9fa37d1
Budnieswski <budnieswski@gmail.com>
2016-04-15 09:12:32 -0300

    Methods comments

--- src/Taraven/Bootstrap.php
+++ src/Taraven/Bootstrap.php
@@ -1,19 +1,69 @@
 <?php
 namespace Taraven;
 
-use Taraven\Wordpress\Cleaner;
 use Timber;
 
 global $timber;
 
 class Bootstrap
 {
-  public function __construct()
+  // Store theme settings, like CSS, JS, Menus ...
+  private $settings;
+  
+
+  /**
+   * Initialize Taraven features
+   * @param boolean $settings [description]
+   */
+  public function __construct($settings = false)
   {
+    // Initializing Timber
     $timber = new Timber();
     Timber::$dirname = 'views';
 
-    echo "Starting fucking Theme ! <br>";
-    echo Cleaner::hello();
+    $this->settingsMerge($settings);
   }
+
+
+  /**
+   * Merge user settings with default settings
+   * @param  [array] $settings [description]
+   * @return [type]           [description]
+   */
+  private function settingsMerge($settings)
+  {
+    $defaultSettings = array(
+      'css'             => array(),
+      'js'              => array(),
+      'menu'            => array(),
+      'acf'             => array(),
+      'google-maps'     => true,
+      'google-jquery'   => true,
+      'blog'            => false,
+    );
+
+    if( is_array($settings) ):
+      foreach ($settings AS $key => $value)
+      {
+        if( is_array($value) )
+          $defaultSettings[$key] = array_merge($defaultSettings[$key], $value);
+        else
+          $defaultSettings[$key] = $value;
+      }
+    endif;
+
+    $this->settings = $defaultSettings;
+    return;
+  }
+
+
+  /**
+   * Get current settings
+   * @return [array] 
+   */
+  public function getSettings()
+  {
+    return $this->settings;
+  }
+
 }
